1b62c2dda7b8b519bf21598bf7206eeca6ef7204
Dejan <dejan.ozebek@gmail.com>
2018-11-18 11:36:10 +0100

    Prvi commit

---
+++ Polmesečni otok/Assets/Collision.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collision : MonoBehaviour {
+    protected static int counter;
+    public Text scoreText;
+    public Text Fulltext;
+    
+
+    private void Start()
+    {
+        counter = 0;
+        scoreText.text = " ";
+        Fulltext.text = " ";
+        SetScoreText();
+        
+        //skret= RFAIPP_Toilet_Closed.GetComponent<Collider>();
+        //papirCol = Papir.GetComponent<Collider>();
+    }
+
+    private void Update() {
+        //Debug.Log(counter);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Destroyables"))
+        {
+            other.gameObject.SetActive(false);
+            counter += 1;
+            SetScoreText();
+        }
+        
+    }
+    
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + counter.ToString();
+        if (counter >= 7)
+        {
+            Fulltext.text = "Vse točke!";
+        }
+    }
+
+
+
+
+
+
+}
---
+++ Polmesečni otok/Assets/Moving.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+
+    private Rigidbody rgbd;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        //rgbd = GetComponent < Rigidbody > ();
+        speed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        //transform.Translate(1f*Time.deltaTime,0f,0f);
+        //Vector3 move = new Vector3(moveHorizontal, moveVertical);
+        //rgbd.AddForce(move * speed);
+        //transform.Translate(speed*Input.GetAxis("Horizontal")* Time.deltaTime, 0f, speed*Input.GetAxis("Vertical")*Time.deltaTime);
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+       
+
+    }
+}
