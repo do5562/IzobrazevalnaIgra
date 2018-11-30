84ae9cc276a89fcd5da144a6ee0693a96d741a25
Dejan <do5562@student.uni-lj.si>
2018-11-30 19:09:06 +0100

    Preverjanje, popravljanje, NAPAKA: IGRALEC LEBDI!

--- Polmesečni otok/Assets/Moving.cs
+++ Polmesečni otok/Assets/Moving.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Moving : MonoBehaviour {
-
+    //public Vector3 vect;
     Rigidbody m_Rigidbody;
-    float m_Speed;
+    public int m_Speed = 20;
+    public float r_Speed = 50.0f;
+    public GameObject player;
+    
+    public Collider col;
 
     void Start()
     {
         //Fetch the Rigidbody component you attach from your GameObject
         m_Rigidbody = GetComponent<Rigidbody>();
         //Set the speed of the GameObject
-        m_Speed = 20.0f;
+        
     }
-
+   
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            //float nov = transform.forward * m_Speed * Time.deltaTime;
             //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            //vect = new Vector3(transform.forward * m_Speed * Time.deltaTime, 0, 0);
+            
             m_Rigidbody.velocity = transform.forward * m_Speed;
+            
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
@@ -32,14 +40,60 @@ public class Moving : MonoBehaviour {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             //Rotate the sprite about the Y axis in the positive direction
-            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * r_Speed, Space.World);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //Rotate the sprite about the Y axis in the negative direction
-            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * r_Speed, Space.World);
         }
-    }
+        // Slope rotation alignment.
 
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (col.Raycast(ray, out hit, 1000))
+        {
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+        }
+        // Auto rotation.
+
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, player.transform.eulerAngles.y, transform.eulerAngles.z);
+
+
+
+
+        // Speed limiter which is not neceesary right now.
+
+        /*
+     
+        if (rigidbody.velocity.magnitude > maxSpeed)
+        {
+            rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+         
+        }
+        */
+
+        // Anti jitter movement solution.
+
+        float step = m_Speed * Time.deltaTime;
+        float folX = (player.transform.position.x - transform.position.x) * step;
+        float folZ = (player.transform.position.z - transform.position.z) * step;
+
+
+        Vector3 v = GetComponent<Rigidbody>().velocity;
+
+        v.x = folX;
+        v.z = folZ;
+
+        GetComponent<Rigidbody>().velocity = v;
+
+    }
+    void FixedUpdate()
+    {
+
+        
+    }
 }
