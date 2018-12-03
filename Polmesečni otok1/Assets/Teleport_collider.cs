98bb98d15ef53252ba9e054926bec0dccf6615f2
KarinZadnik <karin.zadnik@gmail.com>
2018-12-03 11:05:44 +0100

    commit vse ponovno

---
+++ Polmesečni otok1/Assets/Teleport_collider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport_collider : MonoBehaviour {
+    public GameObject player;
+    public Transform teleportTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = teleportTarget.transform.position;
+    }
+
+}
