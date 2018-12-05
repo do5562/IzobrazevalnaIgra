cb6f1849bc424b791a592952d27eb4bf6a552338
KarinZadnik <karin.zadnik@gmail.com>
2018-12-05 11:07:49 +0100

    Popravljeno assets

---
+++ Polmesečni otok/Assets/Moving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+    //public Vector3 vect;
+    Rigidbody m_Rigidbody;
+    public int m_Speed = 20;
+    public float r_Speed = 50.0f;
+    public GameObject player;
+    
+    public Collider col;
+
+    void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        
+    }
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+   
+}
