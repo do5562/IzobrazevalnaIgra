482dde5377146ed3ba047fbc14186610a3594122
Dejan <do5562@student.uni-lj.si>
2018-12-05 16:28:35 +0100

    Praktično končan level 1

---
+++ Polmesečni otok/Assets/DestroyOnTouch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTouch : MonoBehaviour {
+    //  GameObject player;
+    // Use this for initialization
+    private  int tocke;
+    private Moving skripta;
+    private GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player");
+        skripta = player.GetComponent<Moving>();
+        
+        //Debug.Log(tocke);
+// player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+       // Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name=="Player" && tocke >=9)
+        {
+            //Debug.Log("COLLISION");
+            Destroy(gameObject);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        tocke = skripta.tocke;
+        //Debug.Log(tocke);
+    }
+}
--- Polmesečni otok/Assets/Resources/Moving.cs
+++ Polmesečni otok/Assets/Resources/Moving.cs
@@ -1,26 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Moving : MonoBehaviour {
     //public Vector3 vect;
-    
+    public Text scoreText;
+    public Text Fulltext;
     public int m_Speed = 20;
     public float r_Speed = 50.0f;
     public GameObject player;
+    Renderer render;
     Animator animator;
+    Collider m_Collider;
+    
     public Collider col;
-
+    public int tocke;
     void Start()
     {
         animator = player.GetComponent<Animator>();
+        
+        scoreText.text = " ";
+        Fulltext.text = " ";
+        SetScoreText();
+        m_Collider = GameObject.FindGameObjectWithTag("Destroyables").GetComponent<Collider>();
+        render = GameObject.FindGameObjectWithTag("Destroyables").GetComponent<SpriteRenderer>();
+        
+       
         //Fetch the Rigidbody component you attach from your GameObject
         
         //Set the speed of the GameObject
         
     }
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        
+       
+        if (collision.gameObject.CompareTag("Destroyables"))
+        {
+            tocke += 1;
+           
+            SetScoreText();
+            //sprite.enabled = false;
+            collision.gameObject.SetActive(false);
+            m_Collider.enabled = !m_Collider.enabled;
+            
+
+        }
+
+    }
+
+
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
         {
             
@@ -38,10 +71,18 @@ public class Moving : MonoBehaviour {
         transform.Translate(0, 0, z);
 
         
-        
+
+    }
+    public void SetScoreText()
+    {
+        scoreText.text = "Score: " + tocke.ToString();
+        if (tocke >= 7)
+        {
+            Fulltext.text = "Vse točke!";
+        }
     }
     /// <summary>
     /// 
     /// </summary>
-   
+
 }
